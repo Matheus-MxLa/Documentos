199ce82e6e194d064f60c3d7d127c3574ee5477a
Matheus-MxLa <matheuslacerdamaximiano@gmail.com>
2026-03-05 09:32:00 -0300

    J&J-Vue

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Header from './components/header.vue'
+
+</script>
+
+<template>
+  <Header/>
+  <main>
+
+  </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/header.vue
@@ -0,0 +1,18 @@
+<script>
+</script>
+<template>
+<header>
+  <nav>
+    <ul>
+      <li><a href=""></a>aaa</li>
+      <li><a href=""></a>aaa</li>
+      <li><a href=""></a>aaa</li>
+      <li><a href=""></a>aaa</li>
+    </ul>
+  </nav>
+</header>
+</template>
+
+<style scooped>
+
+</style>
